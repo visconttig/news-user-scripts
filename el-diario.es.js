f81c85b6b1a3093c9889680e5ba4f8081170546f
Geronimo Viscontti <visconttig@gmail.com>
2025-07-24 19:26:03 -0400

    ...

--- el-diario.es.js
+++ el-diario.es.js
@@ -31,19 +31,16 @@
     let authors_info = ".info-wrapper";
     let twitter_posts = "figure.embed-container.embed-container--type-twitter";
     let related_articles = "aside.know-more.know-more--with-image";
-    let up_next_videos = "figure.embed-container.embed-container--type-dailymotion.ratio";
+    // let up_next_videos = "figure.embed-container.embed-container--type-dailymotion.ratio"; <=== don't!!! this one hide the videos
     let tags = ".tags";
     let goHomeFloatingButton = "a.go-home__wrapper";
     let extraArticles = ".recirculation-area";
-    // let mostRead = "aside.recirculation-block.recirculation-block--most-viewed-content-by-partners.show-in-mobile.show-in-tablet.show-in-desktop";
-    // let others = ".partner-wrapper"; <==== this one NO !!!
     let redundant_subtitle = "li.subtitle--hasAnchor";
-    // let end_of_article_links = ".c-content"; <==== this one NO !!!
     let errorReportButton = "div#error-report";
     let sponsoredContent = ".sponsored-content-wrapper";
     let comments = "div#edi-comments";
     let footer = ".row.row__footer";
-    let extraSelectors = [header, pre_article_extras, share_buttons, authors_info, twitter_posts, related_articles, up_next_videos, tags, goHomeFloatingButton, extraArticles, redundant_subtitle, errorReportButton, sponsoredContent, comments, footer].join(", ");
+    let extraSelectors = [header, pre_article_extras, share_buttons, authors_info, twitter_posts, related_articles, tags, goHomeFloatingButton, extraArticles, redundant_subtitle, errorReportButton, sponsoredContent, comments, footer].join(", ");
 
 
     const removeElements = () => {
@@ -67,34 +64,34 @@
 })();
 
 
-(function() {
-    'use strict';
+// // (function() {
+// //     'use strict';
 
-    function removePaywallModal() {
-        // 1. Remove the modal
-        const modal = document.querySelector('.tp-modal');
-        if (modal) {
-            modal.remove();
-            console.log('🧼 Removed Piano paywall modal');
-        }
+// //     function removePaywallModal() {
+// //         // 1. Remove the modal
+// //         const modal = document.querySelector('.tp-modal');
+// //         if (modal) {
+// //             modal.remove();
+// //             console.log('🧼 Removed Piano paywall modal');
+// //         }
 
-        // 2. Restore scrolling
-        document.body.style.overflow = 'auto';
-        document.documentElement.style.overflow = 'auto';
+// //         // 2. Restore scrolling
+// //         document.body.style.overflow = 'auto';
+// //         document.documentElement.style.overflow = 'auto';
 
-        // 3. Remove any backdrop/overlay if present
-        const overlay = document.querySelector('.tp-backdrop, .tp-modal-backdrop, .tp-veil'); // guesswork
-        if (overlay) {
-            overlay.remove();
-            console.log('💨 Removed modal overlay');
-        }
-    }
+// //         // 3. Remove any backdrop/overlay if present
+// //         const overlay = document.querySelector('.tp-backdrop, .tp-modal-backdrop, .tp-veil'); // guesswork
+// //         if (overlay) {
+// //             overlay.remove();
+// //             console.log('💨 Removed modal overlay');
+// //         }
+// //     }
 
-    // Run once immediately
-    removePaywallModal();
+// //     // Run once immediately
+// //     removePaywallModal();
 
-    // Run continuously to fight reinjection
-    const observer = new MutationObserver(() => removePaywallModal());
-    observer.observe(document.body, { childList: true, subtree: true });
-})();
+//     // Run continuously to fight reinjection
+//     const observer = new MutationObserver(() => removePaywallModal());
+//     observer.observe(document.body, { childList: true, subtree: true });
+// })();
 
